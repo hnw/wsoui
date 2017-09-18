d57920ed7ee9ce658ee934f97b6c292b045db4e6
Yoshio HANAWA <y@hnw.jp>
2017-09-18 14:15:37 +0900

    Remove non-alphanumeric characters from the organization name.

--- oui-convert.pl
+++ oui-convert.pl
@@ -5,17 +5,31 @@
 #   ./oui-convert.pl manuf > ouidata.go
 #
 
+use strict;
 use MIME::Base64;
+use Text::Unidecode;
+use open ':encoding(utf8)';
+
+my (%key, %org);
 
 while (<>) {
     if (/([\da-f]{2}):([\da-f]{2}):([\da-f]{2})\s+(\S{1,8})\s+/i) {
-        $oui{pack("c", hex($1)).pack("c", hex($2)).pack("c", hex($3))} = $4;
+        my $macaddr = "$1:$2:$3";
+        my $key = encode_base64(pack("ccc", hex($1), hex($2), hex($3)), "");
+        my $org = unidecode($4);
+        $org =~ s/[^-0-9A-Za-z]$//g;
+        $org =~ s/^[^-0-9A-Za-z]//g;
+        $org =~ s/[\&\/\+]/-/g;
+        $org =~ s/[^-0-9A-Za-z]/_/g;
+        next if (length($org) > 8);
+        $key{$macaddr} = $key;
+        $org{$macaddr} = $org;
     }
 }
 
 printf("package wsoui\n\n");
 printf("var oui = map[string]string{\n");
-foreach $k (sort(keys %oui)) {
-    printf("\t\"%s\": \"%s\",\n", encode_base64($k, ""), $oui{$k});
+foreach my $macaddr (sort(keys %org)) {
+    printf("\t\"%s\": \"%s\",\n", $key{$macaddr}, $org{$macaddr});
 }
 printf("}\n");
